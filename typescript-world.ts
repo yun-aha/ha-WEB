9b26c4564aca183639a8b0d876c9ff07bb9d2808
ha-yun <deu05230@naver.com>
2022-04-30 23:19:12 +0900

    add ts-world

--- typescript-world.ts
+++ typescript-world.ts
@@ -1,8 +1,23 @@
-function harry(person: string){
+interface Movie{
+    name: string;
+    age: number;
+    call();
+}
+
+type Character = string[];
+let id:Character = ['Ron','Hermione', 'Draco']
+
+function harry(person: string) : string{
     let a :number = 12;
     return `Hello + ${person} : ${a}age`;
 }
 
-let user : string = "Harry Potter";
+let User : Movie = {
+    name: 'Harry Potter',
+    age: 10,
+    call(){
+        harry(User.name)
+    }
+}
 
-document.body.textContent = harry(user);
+document.body.textContent = User.call();
